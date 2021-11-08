6651f4f6da12d0a857ab199af1d0cf760d4f162b
AritroChowdhury <71551202+AritroChowdhury@users.noreply.github.com>
2021-11-08 20:49:36 +0530

    Add files via upload

---
+++ AppTabNavigator.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { AppStackNavigator } from './AppStackNavigator'
+import TaskAssignScreen from '../screens/TaskAssignScreen';
+
+
+
+export const AppTabNavigator = createBottomTabNavigator({
+  ViewTasks : {
+    screen: AppStackNavigator,
+    navigationOptions :{
+      tabBarIcon : <Image source={require("../assets/home-icon.png")} style={{width:20, height:20}}/>,
+      tabBarLabel : "View Tasks",
+    }
+  },
+  AssignTasks: {
+    screen: TaskAssignScreen,
+    navigationOptions :{
+      tabBarIcon : <Image source={require("../assets/ads-icon.png")} style={{width:20, height:20}}/>,
+      tabBarLabel : "Assign Tasks",
+    }
+  }
+});
